1449bfc4207ee6014de9d368f5fdc15e87c2eb0a
Ben Li <BenL@bxfin.com>
2023-09-23 21:31:27 +1000

    add responsive navbar

--- src/app/rt/rt.module.ts
+++ src/app/rt/rt.module.ts
@@ -1,14 +1,13 @@
 import { NgModule, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LightgalleryModule } from 'lightgallery/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule, } from '@angular/material/toolbar';
 import {MatButtonModule, } from '@angular/material/button';
 import {MatIconModule } from '@angular/material/icon';
 import { RtComponent } from './rt.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FlashComponent } from './home/flash/flash.component';
 import { RtRoutingModule } from './rt-routing.module';
@@ -17,6 +16,10 @@ import { NewsComponent } from './news/news.component';
 import { GuidComponent } from './guid/guid.component';
 import { BuildComponent } from './build/build.component';
 import { BuyComponent } from './buy/buy.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
 
@@ -25,22 +28,31 @@ import { BuyComponent } from './buy/buy.component';
     FormsModule,
     LightgalleryModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
+    // Material
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatSlideToggleModule,
+
+    // Routing
     RtRoutingModule,
   ],
   declarations: [
     RtComponent,
     
-    NavbarComponent,
     HomeComponent,
     FlashComponent,
     SharedComponent,
     NewsComponent,
     GuidComponent,
     BuildComponent,
-    BuyComponent
+    BuyComponent,
+    MainNavComponent
   ],
   exports: [],
   bootstrap: []
